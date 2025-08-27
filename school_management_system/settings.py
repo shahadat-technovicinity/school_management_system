9843ddaf0d9bfa506126febc8ac65bc900d0bed2
shahadat-technovicinity <shahadat.hossain@technovicinity.com>
2025-08-27 14:30:38 +0600

    Update settings.py

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^&@6w!vmgg=4cjp%+!gbzb4b)nwherooji2o+=$f!wng4cx9-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
