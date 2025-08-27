ed6f11d396e19b46b42b950ee7c4f5047de861c7
shahadat-technovicinity <shahadattechnovicinity@gmail.com>
2025-08-27 13:18:44 +0600

    first commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Blog
+from .serializers import BlogSerializer
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
