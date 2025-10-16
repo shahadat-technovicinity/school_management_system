63587d4e8466c1abe6a12e5b3a66557f3bf67193
Mehedi809 <mehedihhasan809.com>
2025-10-17 03:16:34 +0600

    studentprofiledoneproblemsolve

--- student_profile/urls.py
+++ student_profile/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('register_student_profile_info/', StudentProfileCreate.as_view(), name='register'),  #New student form route
-    path('addmissionexamrud/<int:pk>/', StudentProfileUpdateDelete.as_view(), name='delete-admissionexam'),
-
+    path('student_profile_update_delete/<int:pk>/', StudentProfileUpdateDelete.as_view(), name='deletstudent'),
 ]
