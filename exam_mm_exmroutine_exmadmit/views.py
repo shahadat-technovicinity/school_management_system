7fa4c926c42361b36dc86bc3f6f573116ac08dda
Mehedi809 <mehedihhasan809.com>
2025-10-29 03:27:39 +0600

    exam_admit_dashboard_done

--- exam_mm_exmroutine_exmadmit/views.py
+++ exam_mm_exmroutine_exmadmit/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework import generics
 from .models import *
 from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Count, Q
 
 class ExamRoutineListCreateView(generics.ListCreateAPIView):
     queryset = ExamRoutine.objects.all()
@@ -25,3 +28,17 @@ class ExamAdmitListCreateView(generics.ListCreateAPIView):
 class ExamAdmitDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ExamAdmit.objects.all()
     serializer_class = ExamAdmitSerializer
+
+
+
+########## admit card generate summary dashboard api ######
+class AdmitCardSummaryDashboardAPIView(APIView):
+    def get(self, request):
+        generated = ExamAdmit.objects.filter(payment_status='paid').count()
+        pending = ExamAdmit.objects.filter(payment_status='non paid').count()
+
+        data = {
+            'generated': generated,
+            'pending': pending
+        }
+        return Response(data)
