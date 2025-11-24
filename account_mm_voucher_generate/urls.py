6c61d6ea17a877343b481c00425147871bc6c0dd
Mehedi809 <119084888+Mehedi809@users.noreply.github.com>
2025-11-25 00:23:47 +0600

    Update URL path name for voucher generation

--- account_mm_voucher_generate/urls.py
+++ account_mm_voucher_generate/urls.py
@@ -3,7 +3,5 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-      path('vouchers_generate/', GenerateVoucherAPIView.as_view(), name='list-create-voucher'),
-#     # 2. GET Method: To download the generated PDF using the saved voucher ID (pk)
-#     path('finance/voucher/generate/<int:pk>/', VoucherGenerateAPIView.as_view(), name='voucher-pdf-download'),
+      path('vouchers_generate/', GenerateVoucherAPIView.as_view(), name='list_create_voucher'),
 ]
