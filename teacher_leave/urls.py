1edd8e4d44f7e5dd6f83f71cd394e023e0f769cc
Mehedi809 <119084888+Mehedi809@users.noreply.github.com>
2025-12-10 01:55:04 +0600

    Update urls.py

--- teacher_leave/urls.py
+++ teacher_leave/urls.py
@@ -6,16 +6,16 @@ from .views import (
     LeaveBalanceViewSet,
 )
 
-# Teacher Leave Router
-# router = DefaultRouter()
-# router.register(r"leave-types", LeaveTypeViewSet, basename="leave-types")
-# router.register(r"leaves", TeacherLeaveViewSet, basename="teacher-leaves")
-# router.register(r"leave-balances", LeaveBalanceViewSet, basename="leave-balances")
+Teacher Leave Router
+router = DefaultRouter()
+router.register(r"leave-types", LeaveTypeViewSet, basename="leave-types")
+router.register(r"leaves", TeacherLeaveViewSet, basename="teacher-leaves")
+router.register(r"leave-balances", LeaveBalanceViewSet, basename="leave-balances")
 
 urlpatterns = [
     # All endpoints under /teacher/...
     # /teacher/leave-types/
     # /teacher/leaves/
     # /teacher/leave-balances/
-    # path("", include(router.urls)),
+    path("", include(router.urls)),
 ]
