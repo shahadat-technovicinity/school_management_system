d9d70834b194c1a324438729f5ea578445f0a58c
Mehedi809 <119084888+Mehedi809@users.noreply.github.com>
2025-12-17 02:27:11 +0600

    Delete teacher_leave directory

--- teacher_leave/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeacherLeaveConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teacher_leave'
-    verbose_name = 'Teacher Leave Management'
