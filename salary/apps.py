6bb2b54eae0f1501e8dbe1bf6fd39835da184338
Mehedi809 <119084888+Mehedi809@users.noreply.github.com>
2025-12-29 02:28:51 +0600

    Delete salary directory

--- salary/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalaryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'salary'
-    verbose_name = 'Salary Management'
