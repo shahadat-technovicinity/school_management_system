5c891156732b141960307f7a8a5504f34fb7a5d4
imran nur <imrannurint@gmail.com>
2026-02-04 22:27:50 +0600

    update

--- student_profile/views.py
+++ student_profile/views.py
@@ -2,6 +2,7 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
 from .models import *
 from .serializers import *
+
 class StudentProfileCreate(ListCreateAPIView):
     queryset = StudentPersonalInfo.objects.all()
     serializer_class = StudentFullSerializer
