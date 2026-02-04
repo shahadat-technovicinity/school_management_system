4827a9f220d87c3076ab396c5d8f9b73db0f7985
Mehedi809 <mehedihhasan809.com>
2026-02-05 02:36:51 +0600

    homepage contact and later done

--- homepage/serializers.py
+++ homepage/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
+# Home Page Slider Serializer
 class Home_Page_SliderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Home_Page_Slider
@@ -11,4 +12,28 @@ class Home_Page_SliderSerializer(serializers.ModelSerializer):
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
+        fields = '__all__'
+
+
+
+# Admission Notice Serializer
+class AdmissionNoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdmissionNotice
+        fields = '__all__'
+
+
+
+# Contact Message Serializer
+class ContactMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactMessage
+        fields = '__all__'
+
+
+
+# Latter serializer
+class LatterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LetterInfo
         fields = '__all__'
--- homepage/urls.py
+++ homepage/urls.py
@@ -8,4 +8,19 @@ urlpatterns = [
     ## Message urls
     path('messages/', MessageListCreateView.as_view(), name='message-list-create'),
     path('messages/<int:pk>/', MessageRetrieveUpdateDestroyView.as_view(), name='message-detail'),
+
+
+    ## Admission Notice urls
+    path('admissionnotices/', AdmissionNoticeListCreateView.as_view(), name='admissionnotice-list-create'),
+    path('admissionnotices/<int:pk>/', AdmissionNoticeRetrieveUpdateDestroyView.as_view(), name='admissionnotice-detail'),
+
+
+    ## Contact Message urls
+    path('contactmessages/', ContactMessageListCreateView.as_view(), name='contactmessage-list-create'),
+    path('contactmessages/<int:pk>/', ContactMessageRetrieveUpdateDestroyView.as_view(), name='contactmessage-detail'),
+
+
+    ## Letter Info urls
+    path('letterinfos/', LetterInfoListCreateView.as_view(), name='letterinfo-list-create'),
+    path('letterinfos/<int:pk>/', LetterInfoRetrieveUpdateDestroyView.as_view(), name='letterinfo-detail'),
 ]
