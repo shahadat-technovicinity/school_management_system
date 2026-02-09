71098857616d537dfec742c1566814ba818c3446
imran nur <imrannurint@gmail.com>
2026-02-09 22:00:15 +0600

    update apps common base viewset

--- apps/common/views/basemodelview.py
+++ apps/common/views/basemodelview.py
@@ -2,6 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 class BaseModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'post',  'patch', 'delete']
     
