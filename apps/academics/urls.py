fd3b2481c81570daacad48cb4611369e67213485
imran nur <imrannurint@gmail.com>
2026-02-09 19:49:52 +0600

    update apps academics

--- apps/academics/urls.py
+++ apps/academics/urls.py
@@ -5,5 +5,8 @@ from apps.academics.views.class_section import ClassSectionViewSet
 
 router = DefaultRouter()
 router.register("class-sections", ClassSectionViewSet)
+router.register("sections", ClassSectionViewSet, basename="sections")
+router.register("academic-years", ClassSectionViewSet, basename="academic-years")
+router.register("class-rooms", ClassSectionViewSet, basename="class-rooms")
 
 urlpatterns = router.urls
