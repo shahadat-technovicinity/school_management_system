c1e42e8c7cde8a6ed546751a2520aa784a66bc45
imran nur <imrannurint@gmail.com>
2026-02-09 14:48:26 +0600

    update apps

--- apps/enrollments/apps.py
+++ apps/enrollments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EnrollmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enrollments'
+    name = 'apps.enrollments'
