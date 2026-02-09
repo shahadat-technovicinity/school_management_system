1ebcf14c65eacb94263c8c4d690747150a88403a
imran nur <imrannurint@gmail.com>
2026-02-09 22:44:31 +0600

    update apps

--- apps/attendance/serializers/attendance.py
+++ apps/attendance/serializers/attendance.py
@@ -1,21 +1,52 @@
+# attendance/serializers.py
+
 from apps.attendance.models import Attendance
 from rest_framework import serializers
-from academic_class_routine.models import Teacher
-
+from apps.academics.models import ClassSection
+from student_profile.models import StudentPersonalInfo
+from teacher_mm_teacher.models import Teacher
 
 
 class AttendanceRecordSerializer(serializers.Serializer):
-    student = serializers.IntegerField()
+    student = serializers.PrimaryKeyRelatedField(
+        queryset=StudentPersonalInfo.objects.all()
+    )
     status = serializers.ChoiceField(
-        choices=[Attendance.PRESENT, Attendance.ABSENT]
+        choices=Attendance.STATUS_CHOICES
     )
 
 
-class BulkAttendanceRequestSerializer(serializers.Serializer):
-    class_section = serializers.IntegerField()
+class BulkAttendanceSerializer(serializers.Serializer):
+    class_section = serializers.PrimaryKeyRelatedField(
+        queryset=ClassSection.objects.all()
+    )
     date = serializers.DateField()
+    records = AttendanceRecordSerializer(many=True)
     marked_by = serializers.PrimaryKeyRelatedField(
         queryset=Teacher.objects.all()
     )
-    records = AttendanceRecordSerializer(many=True)
 
+    def create(self, validated_data):
+        """
+        UPSERT logic: create or update attendance
+        """
+        teacher = validated_data['marked_by']
+        class_section = validated_data['class_section']
+        date = validated_data['date']
+        records = validated_data['records']
+
+        attendances = []
+
+        for record in records:
+            obj, _ = Attendance.objects.update_or_create(
+                student=record['student'],
+                class_section=class_section,
+                date=date,
+                defaults={
+                    'status': record['status'],
+                    'marked_by': teacher
+                }
+            )
+            attendances.append(obj)
+
+        return attendances
--- apps/attendance/views/attendance.py
+++ apps/attendance/views/attendance.py
@@ -1,35 +1,15 @@
-from apps.attendance.serializers.attendance import BulkAttendanceRequestSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from apps.attendance.services import bulk_mark_attendance
-from apps.academics.models import ClassSection
+# attendance/views.py
 
-class BulkAttendanceAPIView(APIView):
-    def post(self, request):
-        serializer = BulkAttendanceRequestSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+from apps.attendance.serializers.attendance import BulkAttendanceSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-        data = serializer.validated_data
 
-        class_section = ClassSection.objects.get(
-            id=data["class_section"]
-        )
+class BulkAttendanceAPIView(CreateAPIView):
+    serializer_class = BulkAttendanceSerializer
+    permission_classes = [IsAuthenticated]
 
-        result = bulk_mark_attendance(
-            class_section=class_section,
-            date=data["date"],
-            records=data["records"],
-            teacher=data["marked_by"],  
-        )
-
-        return Response(
-            {
-                "message": "Attendance processed",
-                "created": result["created"],
-                "skipped": result["skipped"],
-            },
-            status=status.HTTP_201_CREATED,
-        )
-
-        
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
