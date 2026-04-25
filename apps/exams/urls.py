72af6e958230a4e2975241c8958e919af200cb5d
Imran Nur <imrannurint@gmail.com>
2026-04-25 17:02:33 +0600

    update exams

--- apps/exams/urls.py
+++ apps/exams/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views.core import (
+from .views.setup import (
     ExamSetupViewSet, ExamTypeViewSet, SubjectViewSet, 
-    ExamRoutineViewSet, TeacherDutyViewSet, StudentResultViewSet
+    ExamRoutineViewSet, QuestionBankViewSet
 )
+from .views.logistics import (
+    ExamAdmitCardViewSet, ExamSeatPlanViewSet, TeacherDutyViewSet
+)
+from .views.results import StudentResultViewSet
 
 router = DefaultRouter()
+# Setup
 router.register(r'setup', ExamSetupViewSet)
 router.register(r'types', ExamTypeViewSet)
 router.register(r'subjects', SubjectViewSet)
 router.register(r'routines', ExamRoutineViewSet)
+router.register(r'questions', QuestionBankViewSet)
+
+# Logistics
+router.register(r'admit-cards', ExamAdmitCardViewSet)
+router.register(r'seat-plans', ExamSeatPlanViewSet)
 router.register(r'teacher-duties', TeacherDutyViewSet)
+
+# Results
 router.register(r'results', StudentResultViewSet)
 
 urlpatterns = [
