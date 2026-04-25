80ee5ae49211051c52dfa2457312facbda8d95cd
Imran Nur <imrannurint@gmail.com>
2026-04-25 18:02:03 +0600

    update exams

--- apps/exams/models/results.py
+++ apps/exams/models/results.py
@@ -1,6 +1,6 @@
 from django.db import models
 from apps.common.models_audit import AuditModel
-from .setup import ExamSetup
+from apps.exams.models.setup import ExamSetup
 
 class StudentResult(AuditModel):
     exam_setup = models.ForeignKey(ExamSetup, on_delete=models.CASCADE)
