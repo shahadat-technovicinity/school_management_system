9f060fa78721cccd8ade3455911e0dfbedad9b19
Imran Nur <imrannurint@gmail.com>
2026-04-26 10:41:42 +0600

    update for  swagger schema

--- apps/staff/serializers/profile_serializers.py
+++ apps/staff/serializers/profile_serializers.py
@@ -1,65 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from ..models import StaffProfile, StaffPayroll, LeaveBalance
-
-User = get_user_model()
-
-class UserShortSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'name', 'username']
-
-class StaffPayrollSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StaffPayroll
-        fields = '__all__'
-
-class StaffListSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='user.get_full_name', read_only=True)
-    email = serializers.EmailField(source='user.email', read_only=True)
-
-    class Meta:
-        model = StaffProfile
-        fields = [
-            'id', 'employee_id', 'name', 'email', 'role', 
-            'designation', 'phone', 'joining_date', 'status'
-        ]
-
-class LeaveBalanceSerializer(serializers.ModelSerializer):
-    leave_type_name = serializers.CharField(source='leave_type.name', read_only=True)
-    class Meta:
-        model = LeaveBalance
-        fields = ['id', 'leave_type_name', 'total_allocated', 'used', 'remaining', 'academic_year']
-
-class StaffProfileDetailSerializer(serializers.ModelSerializer):
-    user = UserShortSerializer(read_only=True)
-    payroll = StaffPayrollSerializer(required=False)
-    leave_balances = LeaveBalanceSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = StaffProfile
-        fields = '__all__'
-
-    def create(self, validated_data):
-        payroll_data = validated_data.pop('payroll', None)
-        profile = StaffProfile.objects.create(**validated_data)
-        if payroll_data:
-            StaffPayroll.objects.create(staff=profile, **payroll_data)
-        return profile
-
-    def update(self, instance, validated_data):
-        payroll_data = validated_data.pop('payroll', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        if payroll_data:
-            payroll_instance, _ = StaffPayroll.objects.get_or_create(staff=instance)
-            for attr, value in payroll_data.items():
-                setattr(payroll_instance, attr, value)
-            payroll_instance.save()
-        return instance
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from ..models import (
     StaffProfile, StaffPayroll, LeaveApplication, 
     WorkAssignment, CommitteeMember, LeaveBalance,
@@ -89,16 +29,17 @@ class StaffListSerializer(serializers.ModelSerializer):
             'designation', 'phone', 'joining_date', 'status'
         ]
 
-class LeaveBalanceSerializer(serializers.ModelSerializer):
+class AppsStaffLeaveBalanceSerializer(serializers.ModelSerializer):
     leave_type_name = serializers.CharField(source='leave_type.name', read_only=True)
     class Meta:
         model = LeaveBalance
         fields = ['id', 'leave_type_name', 'total_allocated', 'used', 'remaining', 'academic_year']
+        ref_name = 'AppsStaffLeaveBalanceSerializer'
 
 class StaffProfileDetailSerializer(serializers.ModelSerializer):
     user = UserShortSerializer(read_only=True)
     payroll = StaffPayrollSerializer(required=False)
-    leave_balances = LeaveBalanceSerializer(many=True, read_only=True)
+    leave_balances = AppsStaffLeaveBalanceSerializer(many=True, read_only=True)
     
     class Meta:
         model = StaffProfile
