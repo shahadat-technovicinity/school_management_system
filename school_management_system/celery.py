7a21c9563c45a63779dc7297e49fd9ede126e7a4
amran <imrannurint@gmail.com>
2026-05-26 21:57:31 +0600

    update

--- school_management_system/celery.py
+++ school_management_system/celery.py
@@ -10,6 +10,7 @@ app = Celery('school_management_system')
 # the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
