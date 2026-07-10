e6f30aac730cc3582941bab2837e919ff6641281
imran nur <imrannurint@gmail.com>
2026-07-10 22:53:47 +0600

    update settings

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -412,7 +412,17 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny', 
+    ],
+
+    # 3. Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    
 } 
 
 AUTH_USER_MODEL = 'userauthentication.User'
