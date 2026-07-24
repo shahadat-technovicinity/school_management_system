c5052d003ff735cfd59d6d12336c91d2f8475799
imran nur <imrannurint@gmail.com>
2026-07-25 01:24:17 +0600

    /login update

--- userauthentication/urls.py
+++ userauthentication/urls.py
@@ -8,11 +8,11 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path(
-        "login/",
+        "login_new/",
         CustomTokenObtainPairView.as_view(),
         name="token_obtain_pair",
     ),
-    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('Userlist/', userlistview.as_view(), name='user-list'),
