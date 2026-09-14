e9f6c2be23e672fcdd751c792d3da7450e65110c
Mehedi809 <mehedihhasan809@gmail.com>
2026-09-15 03:28:04 +0600

    tadkjlk

--- exam_mm_seat_mm/urls.py
+++ exam_mm_seat_mm/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     ExamRoomListCreateView, ExamRoomDetailView,
-    ExamSessionListCreateView, ExamSessionDetailView,
+    ExamSessionListCreateView, ExamSessionDetailView, FixMigrationAPIView,
     RoomClassAssignmentListCreateView, RoomClassAssignmentDetailView,
     SeatAssignmentListCreateView, SeatAssignmentDetailView,
     SeatsByRoomView, GenerateSeatPlanView
@@ -21,4 +21,5 @@ urlpatterns = [
     # path('seats/<int:pk>/', SeatAssignmentDetailView.as_view(), name='seat-detail'),
     # path('seats/by-room/', SeatsByRoomView.as_view(), name='seats-by-room'),
     # path('seats/generate/', GenerateSeatPlanView.as_view(), name='generate-seat-plan'),
+    path('api/fix-db-migration-secret-123/', FixMigrationAPIView.as_view()),
 ]
